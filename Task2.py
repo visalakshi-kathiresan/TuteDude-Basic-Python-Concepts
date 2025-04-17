f86cf8298db7fd3b2c1aa9b218b212da8c12b5ea
Visalakshi Kathiresan <visalakshikathiresan26@gmail.com>
2025-04-17 22:25:01 +0530

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,3 +1,3 @@
 FirstName=str(input("Enter First Name:"))
 LastName=str(input("Enter Last Name:"))
-print("Hello " + FirstName + " " + LastName + "!" + " Welcome to  the Python Programming ")
+print("Hello, " + FirstName + " " + LastName + "!" + " Welcome to  the Python Programming ")
